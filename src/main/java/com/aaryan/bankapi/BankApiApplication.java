c3b2ea5b7471f1b8cce236bdfa4a3e7823ad136c
Aaryan Saurabh <aaryan.saurabh007@gmail.com>
2026-04-18 14:10:49 +0530

    Initial project setup with dependencies

---
+++ src/main/java/com/aaryan/bankapi/BankApiApplication.java
@@ -0,0 +1,13 @@
+package com.aaryan.bankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaryan/bankapi/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaryan.bankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
